45d3f6bd764ff2e017945262275cc04af07dcda7
Mhdvi <am3550816@gmail.com>
2026-05-12 09:36:21 +0330

    Create CircleImage.kt

---
+++ CircleImage.kt
@@ -0,0 +1,33 @@
+*/
+ID TELEGRAM : @mhdvia
+/*
+
+package canvas
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
+class CircleImage @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : AppCompatImageView(context, attrs,defStyleAttr) {
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val path = Path()
+
+
+    override fun onDraw(canvas: Canvas) {
+        val radius = width.coerceAtMost(height) / 1.5f
+
+        val x = width / 1.5f
+        val y = height / 1.5f
+
+        path.reset()
+        path.addCircle(x,y,radius, Path.Direction.CW)
+        canvas.clipPath(path)
+        canvas.save()
+
+        super.onDraw(canvas)
+    }
+}
