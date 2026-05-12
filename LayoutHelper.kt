a3cb55e22ea90807e89498fa127812ced0c553bc
Mhdvi <am3550816@gmail.com>
2026-05-12 09:34:29 +0330

    Update LayoutHelper.kt

--- LayoutHelper.kt
+++ LayoutHelper.kt
@@ -1,3 +1,8 @@
+/*
+ID TELEGRAM : @mhdvia
+*/
+
+
 package Animation
 import Animation.AndroidUtilities.dp
 
@@ -13,31 +18,38 @@ object LayoutHelper {
     const val WRAP_CONTENT = ViewGroup.LayoutParams.WRAP_CONTENT
 
     fun createFrame(width: Int, height: Int, gravity: Int = Gravity.NO_GRAVITY, leftMargin: Int, topMargin: Int, rightMargin: Int, bottomMargin: Int): FrameLayout.LayoutParams {
-        val parmsframe = FrameLayout.LayoutParams(width,height)
-        parmsframe.setMargins(AndroidUtilities.dp(leftMargin), AndroidUtilities.dp(topMargin),
-        AndroidUtilities.dp(rightMargin), AndroidUtilities.dp(bottomMargin)
+        val parmsframe = FrameLayout.LayoutParams(dp(width), dp(height))
+        parmsframe.setMargins(
+            AndroidUtilities.dp(leftMargin), AndroidUtilities.dp(topMargin),
+            AndroidUtilities.dp(rightMargin), AndroidUtilities.dp(bottomMargin)
         )
         parmsframe.gravity = gravity
         return parmsframe
-}
-
-
+    }
     fun createLinear(width: Int, height: Int, gravity: Int = Gravity.NO_GRAVITY, leftMargin: Int, topMargin: Int, rightMargin: Int, bottomMargin: Int): LinearLayout.LayoutParams {
-      val parms = LinearLayout.LayoutParams(width, height)
+      val parms = LinearLayout.LayoutParams(dp(width), dp(height))
         parms.gravity = gravity
         parms.setMargins(AndroidUtilities.dp(leftMargin), AndroidUtilities.dp(topMargin), AndroidUtilities.dp(rightMargin), AndroidUtilities.dp(bottomMargin))
         return parms
     }
-
-
-
     fun createText(width: Int, height: Int, gravity: Int = Gravity.NO_GRAVITY, leftMargin: Int, topMargin: Int, rightMargin: Int, bottomMargin: Int): LinearLayout.LayoutParams {
-        val parmstext = LinearLayout.LayoutParams(width,height)
+        val parmstext = LinearLayout.LayoutParams(dp(width), dp(height))
         parmstext.setMargins(dp(leftMargin),dp(topMargin),dp(rightMargin),bottomMargin)
         parmstext.gravity = gravity
         return parmstext
     }
 
+    fun createImage(width: Int, height: Int, gravity: Int = Gravity.NO_GRAVITY, leftMargin: Int, topMargin: Int, rightMargin: Int, bottomMargin: Int) : LinearLayout.LayoutParams {
+        val parmsImage = LinearLayout.LayoutParams(dp(width), dp(height))
+        parmsImage.setMargins(dp(leftMargin),dp(topMargin),dp(rightMargin),dp(bottomMargin))
+        parmsImage.gravity = gravity
+        return parmsImage
+    }
 
-
+    fun createScroll(width: Int, height: Int, gravity: Int = Gravity.NO_GRAVITY, leftMargin: Int, topMargin: Int, rightMargin: Int, bottomMargin: Int) : LinearLayout.LayoutParams {
+        val parmsScroll = LinearLayout.LayoutParams(dp(width), dp(height))
+        parmsScroll.setMargins(dp(leftMargin), dp(topMargin), dp(rightMargin), dp(bottomMargin))
+        parmsScroll.gravity = gravity
+        return parmsScroll
+    }
 }
